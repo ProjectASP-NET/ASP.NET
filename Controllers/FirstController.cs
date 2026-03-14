f7389a6e065f0e66219284ad5369b3a678fb3c5a
Dimass-dev <dmitrii.olaresco@isa.utm.md>
2026-03-14 22:16:21 +0200

    Initial commit: Project setup with Swagger

---
+++ Controllers/FirstController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Control.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FirstController : ControllerBase
+    {
+
+    }
+}
