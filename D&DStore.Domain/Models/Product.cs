5321d32be2e2109fec38d7e207211e5e77590cd5
Dimass-dev <dmitrii.olaresco@isa.utm.md>
2026-04-06 23:23:33 +0300

    Move Git root to Solution level and add architecture layers

---
+++ D&DStore.Domain/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D_DStore.Domain.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+    }
+}
