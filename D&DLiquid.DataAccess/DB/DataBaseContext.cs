65eea59c5552dc93dc0f1fbe622b971eb2f60109
Dimass-dev <dmitrii.olaresco@isa.utm.md>
2026-04-09 16:30:54 +0300

    .

---
+++ D&DLiquid.DataAccess/DB/DataBaseContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using D_DStore.Domain.Entities.Product;
+using Microsoft.EntityFrameworkCore;
+
+namespace D_DStore.DataAccess.DB
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<LiquidData> Liquids { get; set; }
+        public DbSet<VapeData> Vapes { get; set; }
+        public DbSet<ConsumableData> Consumables { get; set; }
+    }
+}
---
+++ D&DLiquid.DataAccess/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D_DLiquid.DataAccess.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(int id);
+        Task<T> CreateAsync(T entity);
+        Task<T?> UpdateAsync(int id, T entity);
+        Task<bool> DeleteAsync(int id);
+    }
+}
