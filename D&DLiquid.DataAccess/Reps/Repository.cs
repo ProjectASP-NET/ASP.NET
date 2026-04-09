056e764d8200ee5db5c717d39284f3dd33dfff9a
Dimass-dev <dmitrii.olaresco@isa.utm.md>
2026-04-09 16:34:38 +0300

    создание методов

---
+++ D&DLiquid.DataAccess/Reps/Repository.cs
@@ -0,0 +1,56 @@
+﻿using D_DLiquid.DataAccess.Interfaces;
+using D_DStore.DataAccess.DB;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D_DLiquid.DataAccess.Reps
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly AppDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public Repository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+            => await _dbSet.ToListAsync();
+
+        public async Task<T?> GetByIdAsync(int id)
+            => await _dbSet.FindAsync(id);
+
+        public async Task<T> CreateAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<T?> UpdateAsync(int id, T entity)
+        {
+            var existing = await _dbSet.FindAsync(id);
+            if (existing is null) return null;
+
+            _context.Entry(existing).CurrentValues.SetValues(entity);
+            await _context.SaveChangesAsync();
+            return existing;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var entity = await _dbSet.FindAsync(id);
+            if (entity is null) return false;
+
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
