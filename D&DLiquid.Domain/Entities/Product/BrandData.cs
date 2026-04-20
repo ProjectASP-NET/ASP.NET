09a9fab98566cd92a04522ff4b0bdb01e43f6fd4
Dimass-dev <dmitrii.olaresco@isa.utm.md>
2026-04-20 17:06:20 +0300

    будет так же связи между брендом и страной производителя

---
+++ D&DLiquid.Domain/Entities/Product/BrandData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Text;
+
+namespace D_DStore.Domain.Entities.Product
+{
+    public class BrandData
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public string? LogoUrl { get; set; }
+
+        public int? CountryId { get; set; }
+        public CountryData? Country { get; set; }
+
+        public ICollection<ProductData> Products { get; set; } = new List<ProductData>();
+    }
+}
---
+++ D&DLiquid.Domain/Entities/Product/CountryData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D_DStore.Domain.Entities.Product
+{
+    public class CountryData
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Code { get; set; } = string.Empty;
+
+        public ICollection<BrandData> Brands { get; set; } = new List<BrandData>();
+    }
+}
