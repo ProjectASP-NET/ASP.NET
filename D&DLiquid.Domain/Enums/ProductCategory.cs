c03b2f3d05d1874815c72e7c3cbe8bf409ebcfde
Dimass-dev <dmitrii.olaresco@isa.utm.md>
2026-04-20 17:12:31 +0300

    Так же категории и теги заместо старых

---
+++ D&DLiquid.Domain/Enums/ProductCategory.cs
@@ -0,0 +1,17 @@
+﻿using D_DStore.Domain.Entities.Product;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D_DStore.Domain.Enums
+{
+    public class ProductCategory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public string? IconUrl { get; set; }
+
+        public ICollection<ProductData> Products { get; set; } = new List<ProductData>();
+    }
+}
---
+++ D&DLiquid.Domain/Enums/ProductTag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using D_DStore.Domain.Entities.Product;
+
+namespace D_DStore.Domain.Enums
+{
+    public class ProductTag
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        public ICollection<ProductData> Products { get; set; } = new List<ProductData>();
+    }
+}
